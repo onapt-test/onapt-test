a142fa3f5c6d885b504c5858795f8b7a103dc0b8
orionivv2 <vladislav.iliev24@gmail.com>
2017-05-28 19:56:23 +0300

    fix

--- web/src/js/controller/main_controller.js
+++ web/src/js/controller/main_controller.js
@@ -1,5 +1,5 @@
 angular.module("app")
-  .controller("mainController", ($scope, $rootScope, $state, $translate)=>{
+  .controller("mainController", ($scope, $rootScope, $state, $translate, $server)=>{
     $rootScope.lang = "ru"
     $rootScope.translate = $scope.translate = $translate
     $scope.setLang = (lang)=>{
